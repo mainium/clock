f777e4bd58a977da7a016a29e8457fdf4b823c30
ZoomQuiet <zoomquiet+git@gmail.com>
2019-10-18 08:19:08 +0800

    👌 IMPROVE: ++eDeliver deps

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Clock.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:logger_file_backend, "~> 0.0.10"},
-
+      {:distillery, "~> 2.0", warn_missing: false},
+      {:edeliver, ">= 1.6.0"},
     ]
   end
 end
